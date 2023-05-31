e11c80c19eb061be2ce85f8e853c38022aac1a34
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-05-31 13:50:04 +0300

    Add Gamer class and new array, additional info

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -8,20 +8,18 @@ public class Main {
                 new Person("John", 12, 113),
                 new Person("Phil", 33, 114),
                 new Person("Frank", 18, 115),
+                new Person("Danny", 28, 116),
         };
 
         Customer[] customers = {
-                new Customer("John", 12, 213, 111321),
-                new Customer("Phil", 33, 214, 111322),
-                new Customer("Frank", 18, 215, 111323),
+                new Customer("John", 12, 113, 111321),
+                new Customer("Phil", 33, 114, 111322),
+                new Customer("Frank", 18, 115, 111323),
         };
 
         Gamer[] gamers = {
-                new Gamer("Sarah", 20, 115, "PlayStation"),
-                new Gamer("John", 12, 116, "Xbox"),
-                new Gamer("Phil", 23, 117, "Nintendo"),
-                new Gamer("Sarah", 18, 118, "PC"),
-
+                new Gamer("Sarah", 30, 112, "PlayStation"),
+                new Gamer("Danny", 28, 116, "PC"),
         };
 
         PrintService printService = new PrintService();
