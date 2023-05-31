2d15a9632c5dbca7de3c23d180c4e2a34ff2ad76
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-05-31 12:44:43 +0300

    Add some changes in domain

--- src/main/java/domain/Main.java
+++ src/main/java/domain/Main.java
@@ -3,18 +3,32 @@ package domain;
 public class Main {
 
     public static void main(String[] args) {
-        Person[] people = {
+        Person[] peoples = {
                 new Person("Sarah", 30, 112),
                 new Person("John", 12, 113),
+                new Person("Phil", 33, 114),
+                new Person("Frank", 18, 115),
         };
-        System.out.println("Размер списка: " + people.length);
-        for (int i = 0; i < people.length; i++) {
-            Person person = people[i];
+        System.out.println("Размер списка: " + peoples.length);
+        for (int i = 0; i < peoples.length; i++) {
+            Person person = peoples[i];
             System.out.println("Имя " + person.getName()
-                    + "; Возраст "
-                    + person.getAge()
+                    + "; Возраст " + person.getAge()
                     + "; Телефон " + person.getPhoneNumber());
         }
-    }
 
+        Customer[] customers = {
+                new Customer("John", 12, 113, 111321),
+                new Customer("Phil", 33, 114, 111322),
+                new Customer("Frank", 18, 115, 111323),
+        };
+        System.out.println("Размер списка покупателей: " + customers.length);
+        for (int i = 0; i < customers.length; i++) {
+            Customer customer = customers[i];
+            System.out.println("Имя " + customer.getName()
+                    + "; Возраст " + customer.getAge()
+                    + "; Телефон " + customer.getPhoneNumber()
+                    + "; Номер карты " + customer.getCardNumber());
+        }
+    }
 }
